3793141f5840c7a00a43863d8babad37faa0622b
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2024-05-27 13:15:12 +0530

    Cors origin updated

--- Controllers/userReg-Log.ts
+++ Controllers/userReg-Log.ts
@@ -2,5 +2,7 @@ import { Request, Response } from "express";
 
 export const userRegister = (req: Request, res: Response):void => {
   const body = req.body;
+  console.log(body);
+  
   res.status(200).json({ message: "User registered successfully", body });
 };
